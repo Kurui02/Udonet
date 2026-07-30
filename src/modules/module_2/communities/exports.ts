4d1e5f18aa52af705222b907668d85823ccd94b9
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-07-29 21:09:21 -0400

    :ambulance: me cago en satanas

---
+++ src/modules/module_2/communities/exports.ts
@@ -0,0 +1,10 @@
+export {
+  getCommunityById,
+  getCommunityBySlug,
+  getAllCommunities,
+  getSubcommunities,
+  getUserMainCommunities,
+  getUserSubcommunities,
+  isUserSubscribed,
+  getCommunityMemberCount,
+} from './services/community.service'
---
+++ src/modules/module_4/notifications/exports.ts
@@ -0,0 +1,2 @@
+// Exportar Servicios (Para que Módulo 3 y Módulo 5 disparen notificaciones)
+export { createNotification } from '../notifications/services/notification.service';
