7d98592f2c66806499106dae9832c3d8060338c2
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-08-01 15:58:30 -0400

    :wrench: chore(modules): add module 5 to main repo.

---
+++ src/modules/module_5/moderation/exports.ts
@@ -0,0 +1,7 @@
+export { verifyContent } from "./utils/content-filter.util";
+export { verifyModeratorPermission } from "./utils/role-verification.util";
+
+export type { VerificationResult } from "./utils/content-filter.util";
+export type { AuthorizationResult } from "./utils/role-verification.util";
+
+export { ThreadManagementService } from "./services/thread-management.service";
